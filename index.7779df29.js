10f169835a781347c1140c0de92ffedab34af189
Marian <getmoodyup@gmail.com>
2024-06-25 20:26:36 +0300

    make build

---
+++ index.7779df29.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(n){n.preventDefault();var a=n.target.closest("a");e.contains(a)&&(t.src=a.href,t.alt=a.title)});
+//# sourceMappingURL=index.7779df29.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const thumbs = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+thumbs.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const link = e.target.closest("a");
+    if (thumbs.contains(link)) {
+        largeImg.src = link.href;
+        largeImg.alt = link.title;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
